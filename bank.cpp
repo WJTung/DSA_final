21a99b68a6ebcd43ba335b83843299c09387b394
WJTung <jie1202@gmail.com>
2015-07-01 11:06:08 +0800

    Try to avoid memory leak in merge

--- bank.cpp
+++ bank.cpp
@@ -109,6 +109,8 @@ pair<int, int> Bank::merge(const char *ID1, const string &password1, const char 
         }
         (i1->second)->Account_history = new_Account_history;
         Account_vector[(i2->second)->create_order] = nullptr;
+        delete (i2->first);
+        delete (i2->second);
         Account_map.erase(i2);
         ans = std::make_pair (SUCCESS, (i1->second)->money);
     }
